d591454c8a681ddcfa8ea4beb23d1afa54b06e71
dhayalan.balaraman <dhayalanit@gmail.com>
2022-12-21 19:06:07 +0530

    First Commit

---
+++ src/main/java/com/example/springbootwithreactivewebfluxopenapicodegen/SpringbootWithReactivewebfluxOpenapiCodegenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithreactivewebfluxopenapicodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithReactivewebfluxOpenapiCodegenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithReactivewebfluxOpenapiCodegenApplication.class, args);
+    }
+
+}
